ad9f4544bc4b4b864a83d7cb64a5dc6f7cc65f40
Alejandro Sánchez Monzón <alejandrosanchezmonzon1@gmail.com>
2023-02-23 11:11:57 +0100

    v2.0

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:codelab/main.dart';
+import 'package:codelab_tutorial_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
